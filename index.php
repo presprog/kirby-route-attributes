c59d28c2082b6b29ba2aa3b53a11d12db942935a
Benedict Massolle <bm@presentprogressive.de>
2021-05-31 10:54:30 +0200

    Include vendor and composer autoload.php to allow for manual zip install

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php declare(strict_types=1);
 
+// Only used when plugin is installed manually via ZIP or GIT submodule.
+@include_once __DIR__ . '/vendor/autoload.php';
+
 use Kirby\Cms\App;
 use PresProg\RouteAttributes\RouteAttributes;
 
---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '467d812b6920c60c5f899266b2667dc86b3914a7',
+    'name' => 'presprog/kirby-route-attributes',
+  ),
+  'versions' => 
+  array (
+    'getkirby/composer-installer' => 
+    array (
+      'pretty_version' => '1.2.1',
+      'version' => '1.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
+    ),
+    'presprog/kirby-route-attributes' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '467d812b6920c60c5f899266b2667dc86b3914a7',
+    ),
+  ),
+);
